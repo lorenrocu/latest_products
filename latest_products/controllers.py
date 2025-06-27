2fcadd573ad3f0f477185b0d5c763c6a08754d4f
Lorenzo <lorenrocu@gmail.com>
2025-06-27 15:43:20 -0500

    Update controllers.py

--- latest_products/controllers.py
+++ latest_products/controllers.py
@@ -2,8 +2,18 @@ from odoo import http
 from odoo.http import request
 
 class LatestProductsController(http.Controller):
+    @http.route('/latest_products/snippet', type='http', auth='public')
+    def get_latest_products(self, **kw):
+        # Buscar todas las listas de precios
+        pricelists = request.env['product.pricelist'].sudo().search([])
+        # Construir una respuesta de texto con nombre e ID
+        response = '\n'.join([
+            f"{pl.id}: {pl.name}" for pl in pricelists
+        ])
+        return response or 'No se encontraron listas de precios.'
+
     @http.route('/latest_products/snippet', type='json', auth='public')
-    def get_latest_products(self):
+    def get_latest_products_json(self):
         pricelist = request.env['product.pricelist'].sudo().search([
             ('name', '=', 'PRECIOS WEB Y ODOO ($-USD) (USD)')
         ], limit=1)
